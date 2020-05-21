ec82b0d3206d3c7d2e461a6b0c3eb08d96946daa
arvind <arvindparkash>
2020-05-21 02:21:05 -0600

    web file changed

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,6 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::post('users/profileadd',function(){
-    return "hello";
-});
-Route::post('users/pro',"UserController@pro");
+
 
 Route::resource('users', "UserController");
